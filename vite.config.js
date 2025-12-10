7fb8141441b68256d7568f0cd988542128b6e3bb
Mu-Solomon <musolomoni@gmail.com>
2025-12-10 18:15:38 +0300

    Counting logic done

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    host: "0.0.0.0", // This binds to all network interfaces
+    port: 5173, // (Optional) Explicitly set the port
+  },
 });
